c62f26138894d569bb067bea1e8b32bf99f134e4
Anuchina Yulia <yuliaanuchina@bk.ru>
2022-08-21 22:09:56 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using RepositoryWorkers;
+
+Repository repository = new ();
+do
+    {
+        Console.WriteLine("Что будем делать? Введите цифру на экран\n1.Выведем на экран\n2.Выведем на экран сотрудника по ID\n" +
+            "3.Удалим сотрудника\n4.Добавим нового сотрудника\n5.Выведем список сотрудников между определенными датами рождения");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        switch (number)
+        {
+            case 1:
+            repository.GetAllWorkers();
+                break;
+
+            case 2:
+            int id;
+            Console.WriteLine("Какое ID смотрим?");
+            string line = Console.ReadLine();
+            while (int.TryParse(line, out id))
+            {
+                Console.WriteLine("неверное число");
+                line = Console.ReadLine();
+            }
+            repository.GetWorkerById(id);
+                break;
+
+            case 3:
+            int idDelete;
+            Console.WriteLine("Какое ID удаляем?");
+            line = Console.ReadLine();
+            while (int.TryParse(line, out idDelete))
+            {
+                Console.WriteLine("неверное число");
+                line = Console.ReadLine();
+            }
+            repository.DeleteWorker(idDelete);
+                break;
+
+            case 4:
+            repository.AddWorker();
+                break;
+            case 5:
+            repository.GetWorkersBetweenTwoDates();
+                break;
+            default:
+                Console.WriteLine("Такой цифры нет");
+                break;
+        }
+
+
+    } while (true);
+
---
+++ Workers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryWorkers
+{
+    public class Workers
+    {
+
+        private int index = 0;
+        private int age;
+        private string name = "";
+        private DateTime dateTime;
+        private DateTime birthDay; 
+
+        public string Name { get { return name; } set { name = value; } }  
+        public DateTime DateTime { get { return dateTime; } set { dateTime = DateTime.Now; } }
+        public DateTime BirthDay
+        {
+            get 
+            { 
+                return dateTime; 
+            }
+            set 
+            {
+                if (value < new DateTime(2010) && value > new DateTime(1930))
+                birthDay = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+
+                age = (dateTime.Year - birthDay.Year);
+            }
+        }
+        public int Index
+        {
+            get { return index; }   
+            set { index++; }
+        }
+
+        public Workers (string name, DateTime birthDay)
+        {
+            Index = index;
+            Age = age;
+            Name = name;
+            BirthDay = birthDay;
+            DateTime = dateTime;
+        }
+    }
+}
